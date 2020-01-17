19f8a31abddb9ae873071bac8774a553d76284b9
YanChimpu <yangqingpu19@sina.com>
2020-01-17 14:27:07 +0800

    add the paser for many files

--- eval.py
+++ eval.py
@@ -1,6 +1,7 @@
 from ap_parser import AveragePrecisionOnImages
 import numpy as np
 import cv2
+import os
 
 
 def get_rectangle_img(img, box, color):
@@ -9,39 +10,66 @@ def get_rectangle_img(img, box, color):
     return img_
 
 
-if __name__ == '__main__':
-    pred_path = '/Users/pu/Documents/work/face_detection/Detection/result/1--Handshaking/' \
-                '1_Handshaking_Handshaking_1_357.txt'
-    gt_path = '/Users/pu/Documents/work/data/wider_face/wider_face_split/wider_face_val/1--Handshaking/' \
-              '1_Handshaking_Handshaking_1_357.txt'
-    img_path = '/Users/pu/Documents/work/data/wider_face/WIDER_val/images/1--Handshaking/' \
-               '1_Handshaking_Handshaking_1_357.jpg'
-    pred_bbx = []
-    f_p = open(pred_path, 'r')
-    for line in f_p.readlines():
-        if line:
-            pred_bbx.append(list(int(i) for i in line.strip().split('\t')))
-    gt_bbx = []
-    f_g = open(gt_path, 'r')
-    for line in f_g.readlines():
-        gt_bbx.append(list(int(i) for i in line.strip().split(' ')))
-    pred_bbx = np.array(pred_bbx)
-    gt_bbx = np.array(gt_bbx)
-    img = cv2.imread(img_path)
-    for box in pred_bbx:
-        img = get_rectangle_img(img, box, color=(255, 0, 0))
-    for box in gt_bbx:
-        img = get_rectangle_img(img, box, color=(0, 255, 0))
-    cv2.imshow('image', img)
-    cv2.waitKey(0)
-    img_name = '1_Handshaking_Handshaking_1_357.jpg'
-    gts = {img_name: gt_bbx}
+def get_pred_dict(Pred_DIR_path):
     predictions = []
-    for box in pred_bbx:
-        info = {'bbox': box, 'confidence': 0.8, 'file_id': img_name}
-        predictions.append(info)
-    num_gt = len(gts[img_name])
-    ap, recall, precision = AveragePrecisionOnImages(gts, predictions, num_gt, min_overlap=0.1, validate_input=True)
+    for dir_path in os.listdir(Pred_DIR_path):
+        for pred_res in os.listdir(pred_DIR_path + dir_path):
+            pred_res_path = pred_DIR_path + dir_path + '/' + pred_res
+            pred_bbx = []
+            f_p = open(pred_res_path, 'r')
+            for line in f_p.readlines():
+                if line:
+                    pred_bbx.append(list(int(i) for i in line.strip().split('\t')))
+            pred_bbx = np.array(pred_bbx)
+            for box in pred_bbx:
+                info = {'bbox': box, 'confidence': 0.8, 'file_id': pred_res[:-4] + '.jpg'}
+                predictions.append(info)
+    return predictions
+
+
+def get_gt_dict(gt_DIR_path_):
+    gts = {}
+    num_gt = 0
+    for dir_path in os.listdir(gt_DIR_path_):
+        for gt_res in os.listdir(gt_DIR_path_ + dir_path):
+            gt_res_path = gt_DIR_path_ + dir_path + '/' + gt_res
+            gt_bbx = []
+            f_g = open(gt_res_path, 'r')
+            for line in f_g.readlines():
+                gt_bbx.append(list(int(i) for i in line.strip().split(' ')))
+            gt_bbx = np.array(gt_bbx)
+            num_gt += len(gt_bbx)
+            img_name = gt_res[:-4] + '.jpg'
+            gts[img_name] = gt_bbx
+    return gts, num_gt
+
+
+def main():
+    predictions = get_pred_dict(pred_DIR_path)
+    gts, num_gt = get_gt_dict(gt_DIR_path)
+    ap, recall, precision = AveragePrecisionOnImages(gts, predictions, num_gt, min_overlap=0.5, validate_input=True)
     print('ap: {}'.format(ap))
     print('recall: {}'.format(recall))
     print('precision: {}'.format(precision))
+
+
+if __name__ == '__main__':
+    pred_DIR_path = 'test/pred/'
+    gt_DIR_path = 'test/gt/'
+    img_DIR_path = '/Users/pu/Documents/work/data/wider_face/WIDER_val/images/'
+    main()
+    #
+    # pred_path = '/Users/pu/Documents/work/face_detection/Detection/result/1--Handshaking/' \
+    #             '1_Handshaking_Handshaking_1_357.txt'
+    # gt_path = '/Users/pu/Documents/work/data/wider_face/wider_face_split/wider_face_val/1--Handshaking/' \
+    #           '1_Handshaking_Handshaking_1_357.txt'
+    # img_path = '/Users/pu/Documents/work/data/wider_face/WIDER_val/images/1--Handshaking/' \
+    #            '1_Handshaking_Handshaking_1_357.jpg'
+    # img = cv2.imread(img_path)
+    # for box in pred_bbx:
+    #     img = get_rectangle_img(img, box, color=(255, 0, 0))
+    # for box in gt_bbx:
+    #     img = get_rectangle_img(img, box, color=(0, 255, 0))
+    # cv2.imshow('image', img)
+    # cv2.waitKey(0)
+
